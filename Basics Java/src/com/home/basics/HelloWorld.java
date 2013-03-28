e384bf2aa81cfe8188d80c4ed37a5ac2fd59ea92
karthick6891 <karthick6891@gmail.com>
2013-03-28 10:14:27 +0530

    First Commit Basics Java

---
+++ Basics Java/src/com/home/basics/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.home.basics;
+
+/**
+ * @author Santhana Rajagopalan S
+ * Mar 28, 2013
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld Java");
+	}
+}
