2c32b8f2b05c0bec717f86d5122afe4f72d86355
karthick6891 <karthick6891@gmail.com>
2013-04-12 22:44:19 +0530

    Init

---
+++ MultiplayerGameHtml5/src/com/multi/controllers/MainController.java
@@ -0,0 +1,74 @@
+package com.multi.controllers;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.multi.commands.Command;
+import com.multi.commands.InitCommand;
+import com.multi.commands.WriteCommand;
+
+/**
+ * Servlet implementation class MainController
+ * @author Santhana Rajagopalan S
+ * Apr 12, 2013
+ */
+@WebServlet("/MainController")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    private Map<String, Command> commands = new HashMap<String, Command>();
+    
+    public void init(){
+        this.commands.put("init",  new InitCommand());
+        this.commands.put("write", new WriteCommand());
+    }
+
+    public void processCommand(HttpServletRequest  request, 
+                               HttpServletResponse response) 
+                           throws ServletException, IOException{
+
+        String formAction = request.getParameter("form_action");
+
+        if(null == formAction){
+            formAction = "init";
+        }
+
+        Command command = (Command)commands.get(formAction);
+
+        if(null == command){
+            throw new IllegalArgumentException(
+                "No command for form action: " + formAction);
+        }
+        command.execute(request, response);
+    }
+
+    public void doPost(HttpServletRequest  request, 
+                       HttpServletResponse response) 
+                  throws ServletException, IOException{
+
+        processCommand(request, response);
+    }
+
+    public void doGet(HttpServletRequest  request, 
+                      HttpServletResponse response) 
+                  throws ServletException, IOException{
+
+        processCommand(request, response);
+    }
+
+}
